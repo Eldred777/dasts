f483932e699b7a4320412cc67d269ac0320f09a9
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2024-12-30 17:32:32 +1000

    Refactor relic drop chance data to enum

--- src/main/java/com/github/eldred777/dasts/stats/relics/DropChance.java
+++ src/main/java/com/github/eldred777/dasts/stats/relics/DropChance.java
@@ -1,33 +1,18 @@
 package com.github.eldred777.dasts.stats.relics;
 
-interface DropChance {
-    // Probabilities of each rarity expressed as a percentage
-    double COMMON = 0;
-    double UNCOMMON = 0;
-    double RARE = 0;
+public enum DropChance {
+    INTACT(25. + 1. / 3., 11., 2.),
+    EXCEPTIONAL(23 + 1. / 3., 13., 4.),
+    FLAWLESS(20., 17., 6.),
+    RADIANT(16. + 2. / 3., 20, 10);
 
-}
+    public final double common;
+    public final double uncommon;
+    public final double rare;
 
-class Exceptional implements DropChance {
-    final double COMMON = 23. + 1. / 3.;
-    final double UNCOMMON = 13.;
-    final double RARE = 4.;
-}
-
-class Flawless implements DropChance {
-    final double COMMON = 20.;
-    final double UNCOMMON = 17.;
-    final double RARE = 6.;
-}
-
-class Intact implements DropChance {
-    final double COMMON = 25. + 1. / 3.;
-    final double UNCOMMON = 11.;
-    final double RARE = 2.;
-}
-
-class Radiant implements DropChance {
-    final double COMMON = 16. + 2. / 3.;
-    final double UNCOMMON = 20;
-    final double RARE = 10;
+    DropChance(double c, double u, double r) {
+        this.common = c;
+        this.uncommon = u;
+        this.rare = r;
+    }
 }
