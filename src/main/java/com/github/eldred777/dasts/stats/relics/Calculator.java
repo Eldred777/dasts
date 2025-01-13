5356a35d7a718442b80091ff62b31a26651c66b5
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2025-01-13 15:13:45 +1000

    Updated/fixed probability computation (PDF->CDF)

--- src/main/java/com/github/eldred777/dasts/stats/relics/Calculator.java
+++ src/main/java/com/github/eldred777/dasts/stats/relics/Calculator.java
@@ -24,27 +24,27 @@ public class Calculator {
     /**
      * Computes the probability with parameter refinement, internal parameter numPeople, evaluated at runNumber.
      * <p>
-     * This is the geometric distribution under the number set {1,2,...} i.e. n-1 failures and the nth trial is a
-     * success.
+     * This is the geometric distribution CDF where the PDF has support {1,2,...} i.e. n-1 failures and the nth trial is
+     * a success.
      *
      * @return Probability as a percentage.
      */
-    private double computePdf(Refinement refinement, int runNumber) {
+    private double computeProb(Refinement refinement, int runNumber) {
         double prob = refinement.get(rarity) / 100;
         double probComplement = 1 - prob;
         if (numPeople > 1) {
             probComplement = Math.pow(probComplement, numPeople);
             prob = 1 - probComplement;
         }
-        return Math.pow(probComplement, runNumber - 1) * prob * 100;
+        return (1 - Math.pow(probComplement, runNumber)) * 100;
     }
 
     public List<Double> computeProbabilities(int n) {
         ArrayList<Double> p = new ArrayList<>();
-        p.add(computePdf(Refinement.INTACT, n));
-        p.add(computePdf(Refinement.EXCEPTIONAL, n));
-        p.add(computePdf(Refinement.FLAWLESS, n));
-        p.add(computePdf(Refinement.RADIANT, n));
+        p.add(computeProb(Refinement.INTACT, n));
+        p.add(computeProb(Refinement.EXCEPTIONAL, n));
+        p.add(computeProb(Refinement.FLAWLESS, n));
+        p.add(computeProb(Refinement.RADIANT, n));
         return p;
     }
 
